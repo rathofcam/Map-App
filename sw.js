7f3004e8d400479a670cb54c1e3067159bc6766f
rathofcam <anneerathge@gmail.com>
2026-04-26 10:08:27 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'map-app-v1';
+const ASSETS = ['index.html', 'manifest.json', 'sw.js', 'icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
